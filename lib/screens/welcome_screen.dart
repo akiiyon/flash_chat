0c0a179b4611593f53ea817d3c95b2e695323bc1
akshay <90518321+akiiyon@users.noreply.github.com>
2025-05-07 23:56:38 +0530

    added TyperWriterAnimation  and dispose method

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -3,6 +3,7 @@
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'registration_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id = 'Welcome_screen';
@@ -14,17 +15,23 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController controller;
-  late Animation animation;
+  late Animation animation; //animation value
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     controller =
         AnimationController(duration: Duration(seconds: 1), vsync: this);
 
-    animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
     controller.forward();
+
+    animation.addStatusListener(
+      (status) {
+        print(status);
+      },
+    );
 
     controller.addListener(() {
       setState(() {});
@@ -33,9 +40,16 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -48,15 +62,17 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: animation.value* 100,
+                    height: 60,
                   ),
                 ),
-                Text(
-                  'Flash chat',
+                DefaultTextStyle(
                   style: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
+                    color: Colors.black,
                   ),
+                  child: AnimatedTextKit(
+                      animatedTexts: [TyperAnimatedText('Flash-chat')]),
                 ),
               ],
             ),
